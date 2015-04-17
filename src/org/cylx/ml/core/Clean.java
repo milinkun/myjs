57754b998553b2d15bf50f1c99e4b1d870aac335
milinkun <canyelengxin@163.com>
2015-04-17 19:28:29 +0800

    init

---
+++ src/org/cylx/ml/core/Clean.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.cylx.ml.core;
+
+/**
+ * desc class function
+ * 
+ * @author <a herf="mailto:chanyelengxin@163.com">milin</a>
+ * 
+ *         2015年4月17日
+ */
+public class Clean {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
